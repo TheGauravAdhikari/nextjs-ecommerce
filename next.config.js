2ea3d95b440ebd81bdcb344abfbfc980c624a36b
Gaurav Adhikari <81704001+TheGauravAdhikari@users.noreply.github.com>
2023-03-10 08:17:47 +0545

    Nextjs-Ecommerce

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    "BASE_URL": "http://localhost:3000",
+    "MONGODB_URL": "mongodb+srv://Sushant:wwe98765@cluster0.c12p4zw.mongodb.net/next_ecommerce?retryWrites=true&w=majority",
+    "ACCESS_TOKEN_SECRET": "vesla12362045355.21062472",
+    "REFRESH_TOKEN_SECRET": "vesla123secret112446585rere48461641",
+    "CLOUD_UPDATE_PRESET": "nextjs_store",
+    "CLOUD_NAME": "dyfawgloj",
+    "CLOUD_API": "https://api.cloudinary.com/v1_1/dyfawgloj/image/upload"
+  }
+  
+}
